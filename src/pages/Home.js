3afb7e1c45835c904f1670d558817f5a6f4f1caa
unknown <you@example.com>
2022-01-03 14:46:13 +0300

    Newsletter component added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import Announcement from '../components/Announcement';
 import Navbar from '../components/Navbar';
 import Categories from '../components/Categories';
 import Products from '../components/Products';
+import Newsletter from '../components/Newsletter';
 
 const Home = () => {
     return (
@@ -12,6 +13,7 @@ const Home = () => {
             <Slider />
             <Categories />
             <Products />
+            <Newsletter />
         </div>
     );
 }
